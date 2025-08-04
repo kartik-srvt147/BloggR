d5d7da3f57a3faac26478afc0f740836a4856349
Kartikeya Srivastava <kartik.srvt@gmail.com>
2025-08-04 12:57:07 +0530

    fixed footer component styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-950 border-t border-teal-800 h-12 flex items-center justify-center text-xs text-gray-500">
+    <div className="bg-gray-900 border-t border-teal-800 h-13 flex items-center justify-center text-xs text-gray-500">
       BloggR &copy; Copyright 2025 | Developed by Kartikeya Srivastava
     </div>
   );
